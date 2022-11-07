75749e8842fd2f2ba968177b234af0e7fa5e2b0c
Skaffa <delanovw2000@gmail.com>
2022-11-07 15:02:44 +0100

    update

--- add.php
+++ add.php
@@ -32,7 +32,7 @@ function storeInput($data)  {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Add Guest</title>
-    <link rel="stylesheet" href="assets/css/add.css">
+    <link rel="stylesheet" href="assets/css/main.css">
 </head>
 <body>
     <div id="container">
--- index.php
+++ index.php
@@ -6,21 +6,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Guestbook</title>
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/main.css">
 </head>
 <body>
-    <a href="add.php">Add</a>
+    <a href="add.php" id="add"><img src="assets/add.svg" width="14px">Add</a>
     <div id="container">
+        <h1 style="text-align: center;">Guestbook</h1>
         <div id="guests">
             <?php
             $guests = json_decode(file_get_contents('guests.json'), true);
             foreach ($guests as $guest) {
-                // $guestData = json_decode(file_get_contents($guest));
                 $name = $guest['name'];
                 $message = $guest['message'];
                 echo('
                 <div class="guest">
-                    <p class="name">'.$name.'</p>
-                    <p class="message">'.$message.'</p>
+                    <p class="name"><img class="svg" src="assets/person.svg">'.$name.'</p>
+                    <p class="message"><img class="svg" src="assets/message.svg">'.$message.'</p>
                 </div>
                 ');
             }
