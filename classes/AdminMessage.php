8efa933d2ed3940290bc94d4faf0b74a800054b3
david-track <david.oudenaarden@outlook.com>
2022-11-11 12:10:30 +0100

    added constructor, getters and setters for messages

--- classes/AdminMessage.php
+++ classes/AdminMessage.php
@@ -6,5 +6,46 @@ class AdminMessage {
     private $timestamp;
     private $userName;
     private $message;
-    
+
+
+    public function __construct(string $id, int $timestamp, string $userName, string $message)
+    {
+        $this->setId($id);
+        $this->setTimestamp($timestamp);
+        $this->setUserName($userName);
+        $this->setMessage($message);
+
+    }
+
+    public function setId(string $id) {
+        $this->id = $id;
+    }
+
+    public function setTimestamp(int $timestamp) {
+        $this->timestamp = $timestamp;
+    }
+
+    public function setUserName(string $userName) {
+        $this->userName = $userName;
+    }
+
+    public function setMessage(string $message) {
+        $this->message = $message;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getTimestamp() {
+        return $this->timestamp;
+    }
+
+    public function getUserName() {
+        return $this->userName;
+    }
+
+    public function getMessage() {
+        return $this->message;
+    }
 }
