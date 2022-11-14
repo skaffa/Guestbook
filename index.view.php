4af9c8d45d6425f148cb2a155338dd902a7a69e1
Skaffa <delanovw2000@gmail.com>
2022-11-14 14:15:57 +0100

    bootstrap

--- index.view.php
+++ index.view.php
@@ -1,14 +1,100 @@
-<link rel="stylesheet" href="https://bootswatch.com/5/lux/bootstrap.css">
-<link rel="stylesheet" href="form.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="style.css">
 
 
 
-<form action="" method="post">
-    <label for="name">Name</label>
-    <input placeholder="Type your name..." class="form-control form-control-lg" name="name" minlength="2" maxlength="50" type="text" required>
-    <label for="message">Message</label>
-    <textarea placeholder="Type your message..." class="form-control" minlength="2" maxlength="500" name="message" id="" cols="30" rows="10" required></textarea>
-    <button class="btn btn-outline-info" type="submit">Submit</button>
-</form>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Guestbook</title>
+</head>
+<body>
+    <div id="container">
+        <form action="" method="post">
+            <div class="form-floating mb-3">
+                <input class="form-control" placeholder="Name" name="name" minlength="2" maxlength="50" type="text" required>
+                <label for="name">Name</label>
+            </div>
+            <div class="form-floating mb-3">
+                <textarea class="form-control" placeholder="Message" minlength="2" maxlength="500" name="message" id="" required></textarea>
+                <label for="message">Message</label>
+            </div>
 
-dwefrgth
+            <button class="btn btn-outline-primary" type="submit">Submit</button>
+        </form>
+        <main>
+
+
+            <article>
+                <div class="message-info">
+                    <div class="name">Rasmus Lerdorf</div>
+                    <div class="time">20 april, 1969 at 13:37</div>
+                </div>
+                <div class="message">Bacon ipsum dolor amet ham prosciutto non, frankfurter qui in turducken eiusmod eu brisket ex pork loin excepteur chuck pig. Shankle cillum capicola tenderloin magna tail sirloin consequat pork belly meatloaf. Chislic swine nulla, tri-tip landjaeger venison flank deserunt labore kevin qui chuck.</div>
+            </article>
+            <article>
+                <div class="message-info">
+                    <div class="name">Rasmus Lerdorf</div>
+                    <div class="time">20 april, 1969 at 13:37</div>
+                </div>
+                <div class="message">Bacon ipsum dolor amet ham prosciutto non, frankfurter qui in turducken eiusmod eu brisket ex pork loin excepteur chuck pig. Shankle cillum capicola tenderloin magna tail sirloin consequat pork belly meatloaf. Chislic swine nulla, tri-tip landjaeger venison flank deserunt labore kevin qui chuck.</div>
+            </article>
+            <article>
+                <div class="message-info">
+                    <div class="name">Rasmus Lerdorf</div>
+                    <div class="time">20 april, 1969 at 13:37</div>
+                </div>
+                <div class="message">Bacon ipsum dolor amet ham prosciutto non, frankfurter qui in turducken eiusmod eu brisket ex pork loin excepteur chuck pig. Shankle cillum capicola tenderloin magna tail sirloin consequat pork belly meatloaf. Chislic swine nulla, tri-tip landjaeger venison flank deserunt labore kevin qui chuck.</div>
+            </article>
+            <article>
+                <div class="message-info">
+                    <div class="name">Rasmus Lerdorf</div>
+                    <div class="time">20 april, 1969 at 13:37</div>
+                </div>
+                <div class="message">Bacon ipsum dolor amet ham prosciutto non, frankfurter qui in turducken eiusmod eu brisket ex pork loin excepteur chuck pig. Shankle cillum capicola tenderloin magna tail sirloin consequat pork belly meatloaf. Chislic swine nulla, tri-tip landjaeger venison flank deserunt labore kevin qui chuck.</div>
+            </article>
+            <article>
+                <div class="message-info">
+                    <div class="name">Rasmus Lerdorf</div>
+                    <div class="time">20 april, 1969 at 13:37</div>
+                </div>
+                <div class="message">Bacon ipsum dolor amet ham prosciutto non, frankfurter qui in turducken eiusmod eu brisket ex pork loin excepteur chuck pig. Shankle cillum capicola tenderloin magna tail sirloin consequat pork belly meatloaf. Chislic swine nulla, tri-tip landjaeger venison flank deserunt labore kevin qui chuck.</div>
+            </article>
+            <article>
+                <div class="message-info">
+                    <div class="name">Rasmus Lerdorf</div>
+                    <div class="time">20 april, 1969 at 13:37</div>
+                </div>
+                <div class="message">Bacon ipsum dolor amet ham prosciutto non, frankfurter qui in turducken eiusmod eu brisket ex pork loin excepteur chuck pig. Shankle cillum capicola tenderloin magna tail sirloin consequat pork belly meatloaf. Chislic swine nulla, tri-tip landjaeger venison flank deserunt labore kevin qui chuck.</div>
+            </article>
+            <article>
+                <div class="message-info">
+                    <div class="name">Rasmus Lerdorf</div>
+                    <div class="time">20 april, 1969 at 13:37</div>
+                </div>
+                <div class="message">Bacon ipsum dolor amet ham prosciutto non, frankfurter qui in turducken eiusmod eu brisket ex pork loin excepteur chuck pig. Shankle cillum capicola tenderloin magna tail sirloin consequat pork belly meatloaf. Chislic swine nulla, tri-tip landjaeger venison flank deserunt labore kevin qui chuck.</div>
+            </article>
+            <article>
+                <div class="message-info">
+                    <div class="name">Rasmus Lerdorf</div>
+                    <div class="time">20 april, 1969 at 13:37</div>
+                </div>
+                <div class="message">Bacon ipsum dolor amet ham prosciutto non, frankfurter qui in turducken eiusmod eu brisket ex pork loin excepteur chuck pig. Shankle cillum capicola tenderloin magna tail sirloin consequat pork belly meatloaf. Chislic swine nulla, tri-tip landjaeger venison flank deserunt labore kevin qui chuck.</div>
+            </article>
+            <article>
+                <div class="message-info">
+                    <div class="name">Rasmus Lerdorf</div>
+                    <div class="time">20 april, 1969 at 13:37</div>
+                </div>
+                <div class="message">Bacon ipsum dolor amet ham prosciutto non, frankfurter qui in turducken eiusmod eu brisket ex pork loin excepteur chuck pig. Shankle cillum capicola tenderloin magna tail sirloin consequat pork belly meatloaf. Chislic swine nulla, tri-tip landjaeger venison flank deserunt labore kevin qui chuck.</div>
+            </article>
+
+
+        </main>
+    </div>
+</body>
+</html>
