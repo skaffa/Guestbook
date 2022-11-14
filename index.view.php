fc95c99222147690eef18ed8ad3efd047bfeb820
Skaffa <delanovw2000@gmail.com>
2022-11-14 11:28:46 +0100

    implementing bootstrap

---
+++ index.view.php
@@ -0,0 +1,8 @@
+<link rel="stylesheet" href="https://bootswatch.com/5/lux/bootstrap.css">
+
+<form action="" method="post">
+    <input name="name" minlength="2" maxlength="50" type="text" required>
+    <!-- <label for="exampleTextarea" class="form-label mt-4"></label> -->
+    <textarea class="form-control" minlength="2" maxlength="500" name="message" id="" cols="30" rows="10" required></textarea>
+    <button class="btn btn-outline-info" type="submit">Submit</button>
+</form>
