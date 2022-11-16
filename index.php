25b7d3dbb79e27ce361ddb3a50932e1ea17a9352
david-track <david.oudenaarden@outlook.com>
2022-11-16 10:32:20 +0100

    update index.php

--- index.php
+++ index.php
@@ -3,5 +3,13 @@
 require "Message.php";
 require "GuestMessage.php";
 
+if (isset($_POST["name"])) {
+    $id = sha1(uniqid());
+    $timestamp = time();
+    $guestMessage = new GuestMessage($id, $timestamp, $_POST["name"], $_POST["message"]);
+
+   var_dump($guestMessage);
+}
+
 require "index.view.php";
 
